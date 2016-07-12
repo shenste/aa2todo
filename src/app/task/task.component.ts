e7511862c6225e90ac8992d48d7871244c7c30d0
Steve Shen <shenste01604@gmail.com>
2016-07-11 23:00:48 -0700

    new component task

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-task',
+  templateUrl: 'task.component.html',
+  styleUrls: ['task.component.css']
+})
+export class TaskComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
